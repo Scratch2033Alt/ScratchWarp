86f78a5a89518bbb5b07921c2c63df8999f587bc
Scratch2033Alt <204460195+Scratch2033Alt@users.noreply.github.com>
2025-05-13 14:25:28 +0700

    Delete addons directory

--- addons/auto-follow-topics/userscript.js
+++
@@ -1,6 +0,0 @@
-export default async function ({ addon, console, msg }) {
-  const subscribeCheckbox = document.querySelector("#id_subscribe");
-  if (subscribeCheckbox && !subscribeCheckbox.checked) {
-    subscribeCheckbox.checked = true;
-  }
-}
--- addons/no-sprite-confirm/userscript.js
+++
@@ -1,8 +0,0 @@
-export default async ({ addon, console }) => {
-  document.body.addEventListener("pointerup", () => {
-    setTimeout(() => {
-      const confirmButton = document.querySelector("[class^='delete-confirmation-prompt_ok-button_']");
-      if (!addon.self.disabled && confirmButton) confirmButton.click();
-    }, 0);
-  });
-};
